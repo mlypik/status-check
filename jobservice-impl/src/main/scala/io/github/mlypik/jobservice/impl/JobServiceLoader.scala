8ee0d4dfa6eb8ec512569506d987c26e8e53718a
emaclyp <maciej.lypik@ericsson.com>
2018-10-03 15:51:34 +0200

    add job service loader

--- jobservice-impl/src/main/scala/io/github/mlypik/jobservice/impl/JobServiceLoader.scala
+++ jobservice-impl/src/main/scala/io/github/mlypik/jobservice/impl/JobServiceLoader.scala
@@ -1,8 +1,9 @@
 package io.github.mlypik.jobservice.impl
 
-import com.lightbend.lagom.scaladsl.api.ServiceLocator
+import com.lightbend.lagom.scaladsl.api.{Descriptor, ServiceLocator}
 import com.lightbend.lagom.scaladsl.api.ServiceLocator.NoServiceLocator
 import com.lightbend.lagom.scaladsl.broker.kafka.LagomKafkaComponents
+import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
 import com.lightbend.lagom.scaladsl.persistence.cassandra.CassandraPersistenceComponents
 import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext, LagomApplicationLoader}
 import com.softwaremill.macwire.wire
@@ -14,6 +15,11 @@ class JobServiceLoader extends LagomApplicationLoader {
   override def load(context: LagomApplicationContext): LagomApplication = new JobsApplication(context) {
     override def serviceLocator: ServiceLocator = NoServiceLocator
   }
+
+  override def loadDevMode(context: LagomApplicationContext): LagomApplication =
+    new JobsApplication(context) with LagomDevModeComponents
+
+  override def describeService: Option[Descriptor] = Some(readDescriptor[JobService])
 }
 
 abstract class JobsApplication(context: LagomApplicationContext)
